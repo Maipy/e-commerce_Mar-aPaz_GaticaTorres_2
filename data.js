01cfa7fd676128a513af6e39c8955ba5b5aadcfb
papa@cebolla.com <papa@cebolla.com>
2024-10-22 21:10:50 -0300

    Sprint 2 completo sin css

---
+++ data.js
@@ -0,0 +1,92 @@
+const data = [
+    {
+        id:1,
+        title:"Pantalones acampanados Steffie",
+        detail:"Pantalón de mezclilla acampanado con detalles de hilo y bolsillos.",
+        detail_2:`Ajuste: acampanado.<br>Tejido: Denim reciclado (stock muerto de fábrica)<br>Largo: 40" <br>Color: Multicolor <br>Lavar a máquina a temperatura inferior a 40 °C o lavar a mano con suavidad. Secar colgado. Evita secar bajo la luz solar directa, ya que los colores pueden desteñirse. El denim reciclado puede desteñirse. Te sugerimos lavarlo por separado o con colores más oscuros.<br>Fabricado en India: Este producto se fabrica en Delhi en una fábrica con salarios justos. La fábrica se especializa en producción a pequeña escala, donde podemos reducir la generación de residuos y gestionar los residuos de la fábrica reciclando los restos para convertirlos en tela de patchwork.<br>Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se fabrica hasta que la materia prima está disponible.`,
+        img:'https://doodlage.in/cdn/shop/files/1_44dd8cd1-075d-4db5-bf43-e85467dfc075.jpg?v=1713000428&width=1000',
+        price: 48_898.28,
+        stock:30,
+        category: "Pantalones"
+    },
+    {
+        id:2,
+        title:"Caftán estampado Blair",
+        detail:"Caftán estampado de gran tamaño con abertura en la espalda.",
+        detail_2: `El caftán viene con una combinación.<br>También puedes combinarlo con nuestro pantalón marrón jade para la temporada festiva.<br>Material: Modal reciclado, tencel y viscosa de bambú (stock de fábrica variado)<br>Color: marrón<br>Tamaño y ajuste<br>Ajuste: extragrande.<br>Altura de la modelo: 1,72 m.<br>El producto está fabricado en talla única.<br>Largo para talla mediana: 122 cm.<br>Instrucciones de cuidado: lavar a máquina a una temperatura inferior a 40 °C o lavar a mano con agua suave. Planchar a temperatura baja y tibia.<br>Fabricado en India: este producto se fabrica en Delhi en una fábrica que cobra salarios justos. La fábrica se especializa en la producción a pequeña escala, donde podemos reducir la generación de desechos y gestionar los desechos de la fábrica reciclando los restos para fabricar telas de patchwork.<br>Nota: Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se produce hasta que la materia prima está disponible.`,
+        img:'https://doodlage.in/cdn/shop/files/8_9ef2dc2d-4f07-4cf6-862a-b676f55eb769.jpg?v=1724763164&width=1000',
+        price: 67_526.19,
+        stock:11,
+        category: "Caftán"
+    },
+    {
+        id:3,
+        title:"Falda con paneles Irene",
+        detail:"Falda larga con paneles y pliegues tipo cuchillo",
+        detail_2: `Bolsillo:  Bolsillo en la costura interior.<br>Ajuste : Entallado en la cintura y ensanchado.<br>Tejido: Algodón reciclado slub/flex/algodón mezclado (stock muerto de fábrica variado)<br>Largo: 42"<br>Color:  Multicolor<br>Cuidado del lavado :Lavar a máquina a temperatura inferior a 40ºC.Lavado a mano suave o a temperatura ambiente. Secar colgado. Evitar secar bajo la luz solar directa ya que los colores pueden desteñirse. <br>Fabricado en India: este producto se fabrica en Delhi en una fábrica que cobra salarios justos. La fábrica se especializa en la producción a pequeña escala, donde podemos reducir la generación de desechos y gestionar los desechos de la fábrica reciclando los restos para fabricar telas de patchwork. <br>Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se fabrica hasta que la materia prima está disponible.<br>PLAZO DE ENTREGA: Esta pieza solo está disponible para pre-pedido por el momento. La entrega puede demorar hasta 45 días.`,
+        img:"https://doodlage.in/cdn/shop/files/55.jpg?v=1695381783&width=800",
+        price:52_391.01,
+        stock:48,
+        category: "Falda"
+    },
+    {
+        id:4,
+        title:"Vestido con paneles Celia",
+        detail:"Vestido plisado ajustado a la cintura con bolsillos laterales.",
+        detail_2: `Abertura:  Cierre de cremallera lateral<br>Bolsillo:  Bolsillo en la entrepierna<br>Ajuste:  Regular<br>Tejido : Algodón reciclado slub/flex/algodón mezclado (stock variado de retales de fábrica)<br>Largo: 43"<br>Color : Multicolor<br>Cuidado del lavado : Lavar a máquina a temperatura inferior a 40ºC.Lavado a mano suave o a temperatura ambiente. Secar colgado. Evitar secar bajo la luz solar directa ya que los colores pueden desteñirse. <br>Fabricado en India: este producto se fabrica en Delhi en una fábrica que cobra salarios justos. La fábrica se especializa en la producción a pequeña escala, donde podemos reducir la generación de desechos y gestionar los desechos de la fábrica reciclando los restos para fabricar telas de patchwork. <br>Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se fabrica hasta que la materia prima está disponible.<br>PLAZO DE ENTREGA: Esta pieza solo está disponible para pre-pedido por el momento. La entrega puede demorar hasta 30 días.` ,
+        img:"https://doodlage.in/cdn/shop/files/IMG_8432.png?v=1695706042&width=800",
+        price:58_212.23,
+        stock:19,
+        category:"Vestido"
+    },
+    {
+        id:5,
+        title:"Vestido coral Arlene",
+        detail:"Vestido batik oversize con paneles sobre residuos de tela reciclada.",
+        detail_2: `Material: Fabricado a partir de diversos materiales de desecho. Obtenemos una variedad de algodón/algodón flameado. <br> Color: Coral <br> Tamaño y ajuste <br>Ajuste: Extra grande<br>Altura del modelo: 5' 6"<br>Largo para mediano: 33 pulgadas<br>Instrucciones de cuidado: lavar a máquina a una temperatura inferior a 40 °C o lavar a mano con agua suave. Planchar a temperatura baja y tibia.<br>Fabricado en India: este producto se fabrica en Delhi en una fábrica que cobra salarios justos. La fábrica se especializa en la producción a pequeña escala, donde podemos reducir la generación de desechos y gestionar los desechos de la fábrica reciclando los restos para fabricar telas de patchwork.<br>Nota: Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se produce hasta que la materia prima está disponible.`,
+        img:"https://doodlage.in/cdn/shop/files/6_cdeb4314-94cc-4e70-a099-70de236c2df6.jpg?v=1724761852&width=800",
+        price:67_526.19,
+        stock:53,
+        category:"Vestido"
+    },
+    {  
+        id:6,
+        title:"Vestido con paneles Karen",
+        detail:"Vestido con paneles, cuello halter para atar y espalda elástica",
+        detail_2:`Ajuste: regular<br>Tejido: algodón reciclado (remanentes de fábrica)<br>Largo: 51"<br>Color: multicolor<br>Cuidado del lavado: lavar a máquina en ciclo frío<br>Hilo: poliéster obtenido de lotes mixtos de restos de fábrica<br>Fabricado en la India: este producto se fabrica en Delhi en una fábrica con salarios justos. La fábrica se especializa en producción a pequeña escala, donde podemos reducir la generación de residuos y gestionar los residuos de fábrica reciclando los restos en tela de patchwork. No se utilizan recursos en la creación, procesamiento, teñido y acabado de los materiales. ¡Solo buenas habilidades artesanales para darle uso a los residuos!<br>Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se fabrica hasta que la materia prima está disponible.`,
+        img:"https://doodlage.in/cdn/shop/products/DL23-DR-002_1.png?v=1695709421&width=800",
+        price:64_033.46,
+        stock:6,
+        category:"Vestido"
+    },
+    {
+        id:7,
+        title:"Falda blanca de Lizzy",
+        detail:"Falda deconstruida con frunces y elástico en espalda con bolsillos laterales en costura interior.",
+        detail_2: `Material: Fabricado a partir de diversos materiales de stock residual. Obtenemos una variedad de popelina, lino y algodón.<br>Color: Un tono de blanco.<br>Tamaño y ajuste<br>Ajuste: Regular<br>Altura del modelo: 5'6"<br>La modelo usa talla S<br>Largo para talla mediana: 32"<br>Instrucciones de cuidado: lavar a máquina a una temperatura inferior a 40 °C o lavar a mano con agua suave. Planchar a temperatura baja y tibia.<br>Fabricado en India: este producto se fabrica en Delhi en una fábrica que cobra salarios justos. La fábrica se especializa en la producción a pequeña escala, donde podemos reducir la generación de desechos y gestionar los desechos de la fábrica reciclando los restos para fabricar telas de patchwork.<br>Nota: Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se produce hasta que la materia prima está disponible.`,
+        img:"https://doodlage.in/cdn/shop/files/DL24-SK-008_1_e321e9ce-cafc-45c3-9b08-fc9563a381a1.jpg?v=1724317817&width=1000",
+        price:44_439.13,
+        stock:25,
+        category:"Falda"
+    },
+    {
+        id:8,
+        title:"Vestido estampado de agosto",
+        detail:"Vestido de tirantes con fruncido en la cintura y bolsillos laterales",
+        detail_2: `Ajuste: Regular<br>Tejido: Algodón reciclado (stock remanente de fábrica)<br>Largo: 50"<br>Color: Púrpura<br>Lavar a máquina a temperatura inferior a 40 °C o lavar a mano con suavidad. Colgar para secar. Evita secar bajo la luz solar directa, ya que los colores pueden desteñirse.<br>Fabricado en India: Este producto se fabrica en Delhi en una fábrica con salarios justos. La fábrica se especializa en producción a pequeña escala, donde podemos reducir la generación de residuos y gestionar los residuos de fábrica reciclando los restos para convertirlos en tela de patchwork.<br>Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se fabrica hasta que la materia prima está disponible.`,
+        img:"https://doodlage.in/cdn/shop/files/printeddress1.jpg?v=1708320975&width=800",
+        price:87_318.35,
+        stock:88,
+        category:"Vestido"
+    },
+    {
+        id:9,
+        title:"Vestido estampado Sarah",
+        detail:"Vestido estampado con detalles en la parte delantera. El vestido viene con un bolsillo lateral y una abertura lateral",
+        detail_2: `Ajuste: Entallado<br>Tejido: Algodón reciclado (stock remanente de fábrica)<br>Largo: 50 "<br>Color: Verde<br>Lavar a máquina a una temperatura inferior a 40 °C o lavar a mano con suavidad. Colgar para secar. Evita secar bajo la luz solar directa, ya que los colores pueden desteñirse.<br>Fabricado en la India: Este producto se fabrica en Delhi en una fábrica que paga salarios justos. La fábrica se especializa en la producción a pequeña escala, donde podemos reducir la generación de residuos y gestionar los residuos de la fábrica reciclando los restos para convertirlos en tela de patchwork.<br>Cada pieza puede venir con un bordado o parche adicional, pero el aspecto general de la pieza no cambia. Estas piezas son parte de una colección de edición limitada, el stock se fabrica hasta que la materia prima está disponible.`,
+        img:"https://doodlage.in/cdn/shop/files/22julydoodla13147-Edit.jpg?v=1692430302&width=800",
+        price:83_825.62,
+        stock:15,
+        category:"Vestido"
+    },
+]
--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const productos = "Productos"
-const verMas = "Ver más información"
+const productos = "Productos";
 document.querySelector("h1").innerText = productos;
-  
-function cartas(){
-    const data = [{"id":1,"title":"Cosmos","detail":"Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.","img":"http://dummyimage.com/450x300.png/5fa2dd/ffffff","price":13,"stock":48},
-    {"id":2,"title":"Sexy Baby","detail":"Phasellus in felis. Donec semper sapien a libero. Nam dui.","img":"http://dummyimage.com/450x300.png/dddddd/000000","price":43,"stock":86},
-    {"id":3,"title":"Starting Over","detail":"Duis aliquam convallis nunc. Proin at turpis a pede posuere nonummy. Integer non velit.\n\nDonec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec pharetra, magna vestibulum aliquet ultrices, erat tortor sollicitudin mi, sit amet lobortis sapien sapien non mi. Integer ac neque.","img":"http://dummyimage.com/450x300.png/dddddd/000000","price":8,"stock":31},
-    {"id":4,"title":"One-Way Ticket to Mombasa (Menolippu Mombasaan)","detail":"In quis justo. Maecenas rhoncus aliquam lacus. Morbi quis tortor id nulla ultrices aliquet.\n\nMaecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui.\n\nMaecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.","img":"http://dummyimage.com/450x300.png/5fa2dd/ffffff","price":36,"stock":63},
-    {"id":5,"title":"Lady on a Train","detail":"Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque.\n\nQuisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla. Nunc purus.","img":"http://dummyimage.com/450x300.png/dddddd/000000","price":42,"stock":74},
-    {"id":6,"title":"Battles Without Honor & Humanity (Jingi naki tatakai)","detail":"Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.\n\nInteger tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat.\n\nPraesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.","img":"http://dummyimage.com/450x300.png/cc0000/ffffff","price":52,"stock":44},
-    {"id":7,"title":"Fall of the Republic: The Presidency of Barack H. Obama","detail":"Vestibulum quam sapien, varius ut, blandit non, interdum in, ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Duis faucibus accumsan odio. Curabitur convallis.\n\nDuis consequat dui nec nisi volutpat eleifend. Donec ut dolor. Morbi vel lectus in quam fringilla rhoncus.","img":"http://dummyimage.com/450x300.png/dddddd/000000","price":84,"stock":9},
-    {"id":8,"title":"Ed Wood","detail":"Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.","img":"http://dummyimage.com/450x300.png/5fa2dd/ffffff","price":62,"stock":87},
-    {"id":9, "title":"Choke","detail":"Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est.\n\nPhasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum.","img":"http://dummyimage.com/450x300.png/dddddd/000000","price":35,"stock":3}]
-    
-    let card = data.map((producto) => `<div class="card mb-3" style="width: 18rem;">
-    <a href="#"><img src="${producto.img}" class="card-img-top img-fluid" style="height: 200px; object-fit: cover;" alt="${producto.id}"></a>
+
+function cartas(data){
+    let card = data.map((producto) => `
+    <div class="card mb-3" style="width: 18rem;">
+      <a href="#"><img src="${producto.img}" class="card-img-top img-fluid" style="height: 200px; object-fit: cover;" alt="${producto.id}"></a>
     <div class="card-body">
     </div>
     <h5 class="card-title">${producto.title}</h5>
@@ -25,6 +15,47 @@ function cartas(){
     </div>
     </div>`);
 
-  document.querySelector("section").innerHTML = card.join().replaceAll(",", "");
+  document.querySelector("section").innerHTML = card.join("")
 }
-cartas();
+cartas(data);
+
+function filterList() {
+  const input = document.querySelector("#search").value.toLowerCase();
+  const filterData = data.filter((prod) => prod.title.toLowerCase().includes(input)
+)
+  if(filterData.length > 0){
+    cartas(filterData)
+  } else{
+    document.querySelector("section").innerText = "El producto buscado no existe"
+  }
+};
+function resetInput() {
+  document.querySelector("#search").value = "";
+  cartas(data);
+}
+document
+    .querySelector("#filter")
+    .addEventListener("click", filterList);
+document.querySelector("#reset").addEventListener("click", resetInput);
+document.addEventListener("DOMContentLoaded", () => {
+    dataCards(data);
+  })
+filterList()
+resetInput()
+function filterCategory(category){
+  if (category === "Todos"){
+    cartas(data);
+  }else if (category ==="Vestido"){
+    const vestido = data.filter(prenda => prenda.category === "Vestido");
+    cartas(vestido);
+  }else if (category ==="Falda"){
+    const falda = data.filter(prenda => prenda.category === "Falda");
+    cartas(falda);
+  }else if (category ==="Caftán"){
+    const caftan = data.filter(prenda => prenda.category === "Caftán");
+    cartas(caftan);
+  }else if (category === "Pantalones"){
+    const pantalones = data.filter(prenda => prenda.category === "Pantalones")
+    cartas(pantalones);
+  }
+}
